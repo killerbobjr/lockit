242175978a63fa56af967484946a9548861c5518
killerbobjr <github@stinkycats.net>
2021-02-18 23:17:42 -0800

    Reworked config loading. Prettified code.

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
-
-var path = require('path');
-var events = require('events');
-var util = require('util');
-var express = require('express');
-var chalk = require('chalk');
-var extend = require('node.extend');
-var Signup = require('lockit-signup');
-var Login = require('lockit-login');
-var ForgotPassword = require('lockit-forgot-password');
-var ChangeEmail = require('lockit-change-email');
-var DeleteAccount = require('lockit-delete-account');
-var utils = require('lockit-utils');
-var configDefault = require('./config.default.js');
+var	path = require('path'),
+	events = require('events'),
+	util = require('util'),
+	express = require('express'),
+	chalk = require('chalk'),
+	extend = require('node.extend'),
+	Signup = require('lockit-signup'),
+	Login = require('lockit-login'),
+	ForgotPassword = require('lockit-forgot-password'),
+	ChangeEmail = require('lockit-change-email'),
+	DeleteAccount = require('lockit-delete-account'),
+	utils = require('lockit-utils'),
+	configDefault = require('./config.default.js');
 
 
 
@@ -24,15 +23,21 @@ var configDefault = require('./config.default.js');
 var Lockit = module.exports = function(config, next)
 {
 	if(!(this instanceof Lockit))
+	{
 		return new Lockit(config, next);
+	}
 
 	this.config = config || {};
 	var that = this;
 
 	if(!this.config.db)
+	{
 		this.database();
-	if(!this.config.emailType || !this.config.emailSettings)
+	}
+	if(!this.config.mail.emailType || !this.config.mail.emailSettings)
+	{
 		this.email();
+	}
 
 	// use default values for all values that aren't provided
 	this.config = extend(true, {}, configDefault, this.config);
@@ -45,41 +50,45 @@ var Lockit = module.exports = function(config, next)
 	this.adapter = this.config.db.adapter || require(db.adapter)(this.config, function(err, db)
 		{
 			if(err)
+			{
 				next(err);
+			}
 			else
 			{
 				// load all required modules
-				var signup = new Signup(that.config, that.adapter);
-				var login = new Login(that.config, that.adapter);
-				var deleteAccount = new DeleteAccount(that.config, that.adapter);
-				var forgotPassword = new ForgotPassword(that.config, that.adapter);
-				var changeEmail = new ChangeEmail(that.config, that.adapter);
+				that.signup = new Signup(that.config, that.adapter);
+				that.login = new Login(that.config, that.adapter);
+				that.deleteAccount = new DeleteAccount(that.config, that.adapter);
+				that.forgotPassword = new ForgotPassword(that.config, that.adapter);
+				that.changeEmail = new ChangeEmail(that.config, that.adapter);
 
 				// send all GET requests for lockit routes to '/index.html'
 				if(that.config.rest)
+				{
 					that.rest();
+				}
 
 				// expose name and email to template engine
 				that.router.use(function(req, res, next)
-				{
-					res.locals.name = req.user?req.user.name || '':'';
-					res.locals.email = req.user?req.user.email || '':'';
-					next();
-				});
+					{
+						res.locals.name = req.user ? req.user.name || '' : '';
+						res.locals.email = req.user ? req.user.email || '' : '';
+						next();
+					});
 
 				// add submodule routes
-				that.router.use(signup.router);
-				that.router.use(login.router);
-				that.router.use(deleteAccount.router);
-				that.router.use(forgotPassword.router);
-				that.router.use(changeEmail.router);
+				that.router.use(that.signup.router);
+				that.router.use(that.login.router);
+				that.router.use(that.deleteAccount.router);
+				that.router.use(that.forgotPassword.router);
+				that.router.use(that.changeEmail.router);
 
 				// pipe events to lockit
-				var emitters = [signup, login, deleteAccount, forgotPassword, changeEmail];
+				var emitters = [that.signup, that.login, that.deleteAccount, that.forgotPassword, that.changeEmail];
 				utils.pipe(emitters, that);
 
 				// special event for quick start
-				signup.on('signup::post', function(user)
+				that.signup.on('signup::post', function(user)
 					{
 						if(that.config.db.url === 'sqlite://' && that.config.db.name === ':memory:')
 						{
@@ -94,7 +103,7 @@ var Lockit = module.exports = function(config, next)
 					});
 
 				events.EventEmitter.call(that);
-				
+
 				next();
 			}
 		});
@@ -109,14 +118,15 @@ util.inherits(Lockit, events.EventEmitter);
  *
  * @private
  */
-Lockit.prototype.database = function() {
-  this.config.db = {
-    url: 'sqlite://',
-    name: ':memory:',
-    collection: 'my_user_table'
-  };
-  var message = 'no db config found. Using SQLite.';
-  console.log(chalk.bgBlack.green('lockit'), message);
+Lockit.prototype.database = function()
+{
+	this.config.db = {
+		url: 'sqlite://',
+		name: ':memory:',
+		collection: 'my_user_table'
+	};
+	var message = 'no db config found. Using SQLite.';
+	console.log(chalk.bgBlack.green('lockit'), message);
 };
 
 
@@ -126,9 +136,10 @@ Lockit.prototype.database = function() {
  *
  * @private
  */
-Lockit.prototype.email = function() {
-  var message = 'no email config found. Check your database for tokens.';
-  console.log(chalk.bgBlack.green('lockit'), message);
+Lockit.prototype.email = function()
+{
+	var message = 'no email config found. Check your database for tokens.';
+	console.log(chalk.bgBlack.green('lockit'), message);
 };
 
 
@@ -138,33 +149,40 @@ Lockit.prototype.email = function() {
  *
  * @private
  */
-Lockit.prototype.rest = function() {
-  var that = this;
-  var __parentDir = path.dirname(module.parent.filename);
+Lockit.prototype.rest = function()
+{
+	var that = this;
+	var __parentDir = path.dirname(module.parent.filename);
 
-  var routes = [
-    this.config.signup.route,
-    this.config.signup.route + '/resend-verification',
-    this.config.signup.route + '/:token',
-    this.config.login.route,
-    this.config.login.logoutRoute,
-    this.config.forgotPassword.route,
-    this.config.forgotPassword.route + '/:token',
-    this.config.changeEmail.route,
-    this.config.changeEmail.route + '/:token',
-    this.config.deleteAccount.route,
-  ];
+	var routes = [
+		this.config.signup.route,
+		this.config.signup.resendRoute,
+		this.config.signup.resendRoute + '/:token',
+		this.config.login.route,
+		this.config.login.logoutRoute,
+		this.config.forgotPassword.route,
+		this.config.forgotPassword.route + '/:token',
+		this.config.changeEmail.route,
+		this.config.changeEmail.route + '/:token',
+		this.config.deleteAccount.route,
+	];
 
-  routes.forEach(function(route) {
-    that.router.get(route, function(req, res) {
-      // check if user would like to render a file or use static html
-      if (that.config.rest.useViewEngine) {
-        res.render(that.config.rest.index, {
-          basedir: req.app.get('views')
-        });
-      } else {
-        res.sendfile(path.join(__parentDir, that.config.rest.index));
-      }
-    });
-  });
+	routes.forEach(function(route)
+		{
+			that.router.get(route, function(req, res)
+				{
+					// check if user would like to render a file or use static html
+					if(that.config.rest.useViewEngine)
+					{
+						res.render(that.config.rest.index,
+							{
+								basedir: req.app.get('views')
+							});
+					}
+					else
+					{
+						res.sendfile(path.join(__parentDir, that.config.rest.index));
+					}
+				});
+		});
 };
